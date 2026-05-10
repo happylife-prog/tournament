daf0954a0aa4dfc792e5d57bbf21a5c9f6ceeba6
happylife-prog <ooyama2423@gmail.com>
2026-05-10 14:54:53 +0900

    Initial commit: トーナメントアプリ (PWA)

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'tournament-v1';
+const FILES = [
+  './tournament.html',
+  './manifest.json',
+  './icon.svg',
+  './icon-maskable.svg',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES.slice(0, 4))) // Google Fonts はネットワーク優先
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
